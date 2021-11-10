2e859385ffdab6d9107f0edfebe902231a3f5322
ComradEvg <88490590+ComradEvg@users.noreply.github.com>
2021-11-10 20:33:32 +0300

    фиксация 'main'

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+const { src, dest, watch, parallel } = require('gulp');
+const scss = require('gulp-sass')(require('sass'))
+const concat = require('gulp-concat')
+const browserSync = require('browser-sync').create();
+const uglify = require('gulp-uglify-es').default;
+const autoprefixer = require('gulp-autoprefixer');
+
+function browsersync(){
+   browserSync.init({
+      server: {
+         baseDir: 'app/'
+      }
+   })
+}
+
+function scripts(){ 
+   return src([
+      '',
+      'app/js/main.js'
+   ])
+   .pipe(concat('main.min.js'))
+   .pipe(uglify())
+   .pipe(dest('app/js'))
+   .pipe(browserSync.stream())
+}
+
+function styles(){
+   return src('app/scss/style.scss')
+   .pipe(scss({outputStyle: 'compressed'}))
+   .pipe(concat('style.min.css'))
+   .pipe(autoprefixer({
+      overrideBrowserslist: ['last 10 version'],
+      grid: true
+   }))
+   .pipe(dest('app/css'))
+   .pipe(browserSync.stream())
+}
+
+function watching(){
+   watch(['app/scss/**/*.scss'], styles)
+   watch(['app/js/**/*.js','!app/js/main.min.js'], styles)
+   watch(['app/*.html']).on('change', browserSync.reload)
+}
+
+function build(){
+   return src([
+      'app/css/style.css',
+      'app/fonts/**/*',
+      'app/js/main.min.js',
+      'app/*.html',
+      'app/images/**/* '
+   ], {base: 'app'})
+   .pipe(dest('dist'))
+}
+
+exports.styles = styles;
+exports.watching = watching;
+exports.browsersync = browsersync;
+exports.scripts = scripts;
+exports.build = build;
+
+exports.default = parallel(scripts, browsersync, watching);
